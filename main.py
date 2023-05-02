0b1c120eb5d607f01b2f3d0727cdd6e833e5299e
Pud-of-Mud <109466200+Pud-of-Mud@users.noreply.github.com>
2023-05-02 14:28:38 +0000

    Prepairing for merge to main

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
   
   # Uncomment the following line when working with developmental 
   # branches; however re-comment before merging to main. 
-  print(Fore.RED + 'Developer Build!\n' + Style.RESET_ALL)
+  #print(Fore.RED + 'Developer Build!\n' + Style.RESET_ALL)
   print(Fore.RED + "WARNING: All engines cannot recall from memory ATM.\n"  + Style.RESET_ALL)
 
   # API key is kept in memory and never stored locally. 
